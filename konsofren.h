93f40931004d5ccd264d99b9a95c089cef7e39aa
konaki <konaki@anokonaki.org>
2026-07-25 11:05:51 +0800

    add tmp to resizeFramebuffer function

--- konsofren.h
+++ konsofren.h
@@ -21,44 +21,54 @@ void kon_resizeFramebuffer(kon_framebuffer_t *fb, int width, int height);
 
 kon_framebuffer_t *kon_createFramebuffer(int width, int height) {
 	kon_framebuffer_t *fb = malloc(sizeof(kon_framebuffer_t));
+	if (!fb) return NULL;
+
+	fb->data = malloc(width * height * sizeof(uint32_t));
+	if (!fb->data) {
+		free(fb);
+		return NULL;
+	}
+
 	fb->width = width;
 	fb->height = height;
-	fb->data = malloc(width * height * sizeof(uint32_t));
 
 	kon_clearFramebuffer(fb, 0x000000FF);
 	return fb;
 }
 
 void kon_freeFramebuffer(kon_framebuffer_t *fb) {
-	if (fb) {
-		free(fb->data);
-		free(fb);
-	}
+	if (!fb) return;
+
+	free(fb->data);
+	free(fb);
 }
 
 void kon_putPixel(kon_framebuffer_t *fb, int x, int y, uint32_t color) {
-	if (fb) {
-		if (x < 0 || x >= fb->width || y < 0 || y >= fb->height) return;
-		fb->data[y * fb->width + x] = color;
-	}
+	if (!fb) return;
+
+	if (x < 0 || x >= fb->width || y < 0 || y >= fb->height) return;
+	fb->data[y * fb->width + x] = color;
 }
 
 void kon_clearFramebuffer(kon_framebuffer_t *fb, uint32_t color) {
-	if (fb) {
-		for (int i = 0; i < fb->width * fb->height; i++) {
-			/* did it directly to not check overhead because of the if in bounds check */
-			fb->data[i] = color;
-		}
+	if (!fb) return;
+
+	for (int i = 0; i < fb->width * fb->height; i++) {
+		/* did it directly to not check overhead because of the if in bounds check */
+		fb->data[i] = color;
 	}
 }
 
 void kon_resizeFramebuffer(kon_framebuffer_t *fb, int width, int height) {
 	if (!fb) return;
 
+	uint32_t *tmp = realloc(fb->data, (size_t)width * height * sizeof(uint32_t));
+	if (!tmp) return;
+
+	db->data = tmp;
 	fb->width  = width;
 	fb->height = height;
 
-	fb->data = realloc(fb->data, (size_t)width * height * sizeof(uint32_t));
 	kon_clearFramebuffer(fb, 0x000000FF);
 }
 
