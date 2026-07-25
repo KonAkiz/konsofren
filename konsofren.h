d1c208e98c5e0a305baf005c25b4f7a2fc44fd42
konaki <konaki@anokonaki.org>
2026-07-25 09:15:21 +0800

    implement framebuffer create and free functions

--- konsofren.h
+++ konsofren.h
@@ -13,4 +13,25 @@ kon_framebuffer_t *kon_createFramebuffer(int width, int height);
 void kon_freeFramebuffer(kon_framebuffer_t *fb);
 void kon_putPixel(kon_framebuffer_t *fb, int x, int y, uint32_t color);
 
+/*** implementation ***/
+
+#ifdef KONSOFREN_IMPLEMENTATION
+
+kon_framebuffer_t *koncreateFramebuffer(int width, int height) {
+	kon_framebuffer_t *fb = malloc(sizeof(kon_framebuffer_t));
+	fb->width = width;
+	fb->height = height;
+	fb->data = malloc(width * height * sizeof(uint32_t));
+	return fb;
+}
+
+void kon_freeFramebuffer(kon_framebuffer_t *fb) {
+	if (fb) {
+		free(fb->data);
+		free(fb);
+	}
+}
+
+#endif
+
 #endif
