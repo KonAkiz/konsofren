6ca31acfdb598dd233b9599addb2f0639e134279
konaki <konaki@anokonaki.org>
2026-07-25 09:14:17 +0800

    make functions for framebuffer

--- konsofren.h
+++ konsofren.h
@@ -2,10 +2,15 @@
 #define KONSOFREN_H
 
 #include <stdint.h>
+#include <stdlib.h>
 
-struct kon_framebuffer {
+typedef struct kon_framebuffer {
 	int width, height;
 	uint32_t *data;
-};
+} kon_framebuffer_t;
+
+kon_framebuffer_t *kon_createFramebuffer(int width, int height);
+void kon_freeFramebuffer(kon_framebuffer_t *fb);
+void kon_putPixel(kon_framebuffer_t *fb, int x, int y, uint32_t color);
 
 #endif
